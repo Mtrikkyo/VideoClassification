c3f1112dc70de19d5a3f710c9aa6d137cea2c40d
Mtrikkyo <tsubasa.masumura@icoud.com>
2023-12-24 17:15:53 +0900

    update main.sh

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@ python main.py \
 --epoch 100 \
 --random_state 42 \
 --batch_size 64 \
---data_dir /home/tsubasa/Competition/Rikkyo/DeePLearningCompetition/data \
---save_dir /home/tsubasa/Competition/Rikkyo/DeePLearningCompetition/result/custom/vgg3d13/relu/lr1e-3 \
+--data_dir /Data/data \
+--save_dir /Data/result \
 --train_size 0.7 \
 --learning_rate 0.001 \
